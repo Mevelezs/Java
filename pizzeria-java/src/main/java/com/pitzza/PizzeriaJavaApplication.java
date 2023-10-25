e045d05bb81c1a4e1bbc5dbfafddd81a7910adfb
Mauricio Vélez <serketh7@gmail.com>
2023-10-25 01:33:39 -0500

    init new spring boot project 'pizzeria.java'

---
+++ pizzeria-java/src/main/java/com/pitzza/PizzeriaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pitzza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaJavaApplication.class, args);
+	}
+
+}
---
+++ pizzeria-java/src/test/java/com/pitzza/PizzeriaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitzza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
