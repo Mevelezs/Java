02f775b67dba211f815a54aac19d661a9c6024b9
Mauricio Vélez <serketh7@gmail.com>
2023-11-03 23:48:30 -0500

    start autentications & configuring CORS

--- pizzeria-java/src/main/java/com/pitzza/persistence/projection/OrderSummary.java
+++ pizzeria-java/src/main/java/com/pitzza/persistence/projection/OrderSummary.java
@@ -1,7 +1,7 @@
 package com.pitzza.persistence.projection;
 
 import java.time.LocalDateTime;
-
+/*Las proyecciones son para obtener datos de varias tablas sin tener que traer toda la entidad; son resultados personalizados*/
 public interface OrderSummary {
   Integer getIdOrder();
   String getCustomerName();
--- pizzeria-java/src/main/java/com/pitzza/web/controllers/PizzaController.java
+++ pizzeria-java/src/main/java/com/pitzza/web/controllers/PizzaController.java
@@ -30,6 +30,7 @@ public class PizzaController {
   }
 
   @GetMapping ( "/available" )
+  @CrossOrigin (origins = "http://127.0.0.1:5500")
   public ResponseEntity< Page< PizzaEntity > > getAvailable (
           @RequestParam (defaultValue = "0") int page,
           @RequestParam (defaultValue = "8") int elements,
