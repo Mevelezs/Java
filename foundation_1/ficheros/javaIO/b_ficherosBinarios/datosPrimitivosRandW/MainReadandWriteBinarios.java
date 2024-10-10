8772b448ca3272a5b39d8d22021bf87f07ecba9e
mevelezs <serketh7@gmail.com>
2024-10-10 00:37:20 -0500

    java manejo de ficheros - ficheros binarios

--- foundation_1/ficheros/javaIO/b_ficherosBinarios/datosPrimitivosRandW/MainReadandWriteBinarios.java
+++ foundation_1/ficheros/javaIO/b_ficherosBinarios/datosPrimitivosRandW/MainReadandWriteBinarios.java
@@ -23,13 +23,43 @@ public class MainReadandWriteBinarios {
 			DataOutputStream fis = new DataOutputStream(new FileOutputStream((fileName))); // creando el fichero binario
 			fis.writeUTF("Data");//escribiendo un String en utf
 
-			FileInputStream fis1 = new FileInputStream(fileName); // creando la instancia para leer
+			FileInputStream fis1 = new FileInputStream(fileName); // creando la instancia para leer el fichero
 			int num;
 			System.out.println("Datos leidos del archivo");
 			// El metodo read permite leer bit a bit (manda el valor ASCII del caracter y -1 cuando es vacío)
 			while ( ( num = fis1.read() ) != -1 ) System.out.print(num + " ");
 
 			fis.close(); // cerramos el fichero
+		} catch ( IOException e ){
+			throw new RuntimeException(e);
+		}
+
+		//Escribiendo y leyendo distintos tipos de primitivos en un archivo binario
+		String fileBin2 = "/home/mevelezs/Downloads/ensayoJava/archivo2.dat";
+		boolean approve = true;
+		String name = "Mauricio";
+		int age = 39;
+		double height = 1.68;
+
+		try{
+			FileOutputStream fos = new FileOutputStream(fileBin2);
+			// Escribiendo -> pasamos un buffer para mejora la eficiencia
+			DataOutputStream out = new DataOutputStream(new BufferedOutputStream(fos));
+			out.writeBoolean(approve);
+			out.writeUTF(name);
+			out.writeInt(age);
+			out.writeDouble(height);
+			out.close();
+
+			System.out.println("\n---------------------------------------------");
+			//Leyendo -> Se tiene que saber el orden de los datos para poder leerlos (no se lee en cualquier orden)
+			DataInputStream in = new DataInputStream(new FileInputStream(fileBin2));
+			System.out.println("Reading value approve: "+ in.readBoolean());
+			System.out.println("Reading value name: "+ in.readUTF());
+			System.out.println("Reading value age: "+ in.readInt());
+			System.out.println("Reading value height: "+ in.readDouble());
+			in.close();
+
 		} catch ( IOException e ){
 			throw new RuntimeException(e);
 		}
